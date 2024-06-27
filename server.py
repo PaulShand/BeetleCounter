5bca489536a14f611c11c2a8779fd5fba849724a
PaulShand <130605786+PaulShand@users.noreply.github.com>
2024-06-27 02:02:49 -0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify, send_file
+from werkzeug.utils import secure_filename
+from BeetleCounter import count_beetles
+import os
+import cv2
+
+# Starts Flask app
+app = Flask(__name__)
+
+# Enforce Folders are made
+UPLOAD_FOLDER = 'uploads'
+PROCESSED_FOLDER = 'processed'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(PROCESSED_FOLDER, exist_ok=True)
+
+# Prints out traffic
+@app.before_request
+def log_request_info():
+    print(f"Request Headers: {request.headers}")
+
+# Accepts POST from route /upload
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # Checks a file is in the request
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files['file']
+
+    # Ensure the file not empty
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+    
+    # Save the original file
+    filename = secure_filename(file.filename)
+    file_path = os.path.join(UPLOAD_FOLDER, filename)
+    file.save(file_path)
+
+    # Process the image
+    beetle_count, processed_image = count_beetles(file_path)
+
+    # Save the processed image
+    processed_filename = f"processed_{filename}"
+    processed_file_path = os.path.join(PROCESSED_FOLDER, processed_filename)
+    cv2.imwrite(processed_file_path, processed_image)
+
+    # Gives off the place where the prcessed file will be found
+    return jsonify({
+        "message": "File uploaded and processed successfully",
+        "processed_file_url": f"/processed/{processed_filename}",
+        "beetle_count": beetle_count
+    }), 200
+
+# Receives the image at specific location
+@app.route('/processed/<filename>', methods=['GET'])
+def get_processed_file(filename):
+    return send_file(os.path.join(PROCESSED_FOLDER, filename))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

ac2bfc02db27e4863db3025191a5a53af9e4419d
PaulShand <130605786+PaulShand@users.noreply.github.com>
2024-07-03 13:01:02 -0700

    Added black bar to keep image square, Better Numbering, and Better False positive detection

--- BeetleCounter.py
+++ BeetleCounter.py
@@ -31,6 +31,11 @@ def count_beetles(image_path):
         if stats[i, cv2.CC_STAT_AREA] >= 20:
             filtered_binary[labels == i] = 255
 
+    # Dilate the binary image to slightly enlarge the objects
+    kernel = np.ones((3, 3), np.uint8)  # Adjust the kernel size as needed
+    filtered_binary = cv2.dilate(filtered_binary, kernel, iterations=2)
+
+
     # Get height and width of image
     h, w = filtered_binary.shape[:2]
     # Mask needs to be 2 pixels larger than the image
@@ -73,10 +78,13 @@ def count_beetles(image_path):
     # Get number of contours
     final_beetle_count = len(contours)
 
+    font_scale = min(h, w) / 1000  # Adjust the divisor to change the relative size of the font
+    thickness = max(1, int(min(h, w) / 300))
+
     for idx, contour in enumerate(contours):
         # Draw the contour
         cv2.drawContours(image, [contour], -1, (0, 0, 255), 3)
-
+        
         # Calculate the center of the contour for placing the number
         M = cv2.moments(contour)
         if M["m00"] != 0:
@@ -84,9 +92,34 @@ def count_beetles(image_path):
             cY = int(M["m01"] / M["m00"])
         else:
             cX, cY = 0, 0
-
+        
+        # Ensure the text is within image bounds
+        text = str(idx + 1)
+        (text_width, text_height), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, font_scale, thickness)
+        cX = max(cX, text_width // 2)
+        cY = max(cY, text_height // 2)
+        cX = min(cX, w - text_width // 2)
+        cY = min(cY, h - text_height // 2)
+        
         # Add the number on top of the contour
-        cv2.putText(image, str(idx + 1), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 255), 5)
+        cv2.putText(image, text, (cX - text_width // 2, cY + text_height // 2), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (0, 255, 255), thickness)
 
-    return final_beetle_count, image
+
+
+
+    h, w = filtered_binary.shape[:2]
+
+    # Calculate the size of the new square image
+    new_size = max(h, w)
+
+    # Calculate padding for each side
+    pad_top = (new_size - h) // 2
+    pad_bottom = new_size - h - pad_top
+    pad_left = (new_size - w) // 2
+    pad_right = new_size - w - pad_left
+
+    square_image = cv2.copyMakeBorder(image, pad_top, pad_bottom, pad_left, pad_right, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+
+
+    return final_beetle_count, square_image
 
